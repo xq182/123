adb13bd27d82ccde2d42165ee47582a6a4e9b788
xq182 <96278162+xq182@users.noreply.github.com>
2022-12-15 20:35:03 +0800

    Site updated: 2022-12-15 20:35:02

--- js/visit.js
+++ js/visit.js
@@ -1,2 +1,2 @@
-document.getElementById('busuanzi_value_site_pv').innerHTML= '1345678';
-document.getElementById('busuanzi_value_site_uv1').innerHTML= '56326788';
+document.getElementById('busuanzi_value_site_pv1').innerHTML= '56326788';
+document.getElementById('busuanzi_value_site_uv1').innerHTML= '3526782';
